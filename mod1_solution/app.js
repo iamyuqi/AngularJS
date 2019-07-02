fb69e61ae074bfc7bfdee1494f7f31c9b3e0af56
Yuqi Liu <yliu176@stevens.edu>
2019-07-02 14:34:52 -0400

    module 1 update

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -9,23 +9,33 @@ function MsgController($scope) {
     $scope.items = "";
     $scope.totalItems = 0;
     $scope.msg = "";
+    $scope.style = {color: 'black'};
 
     $scope.displayNumeric = function () {
-        var totalSItms = $scope.items.split(",").length;
+        var totalSItms = CalculatItemNumber($scope.items);
         $scope.totalItems = totalSItms;
     };
 
     $scope.lunchCheck = function () {
         if($scope.totalItems == 0){
             $scope.msg = "Please enter data first";
+            $scope.style = {color: 'red'};
         }
         else if($scope.totalItems < 4){
             $scope.msg = "Enjoy!";
+            $scope.style = {color: 'green'};
         }
         else{
             $scope.msg = "Too Much!";
+            $scope.style = {color: 'green'};
         }
     };
 }
-
+    function CalculatItemNumber(string){
+        var totalItemNumber = 0;
+        for (var i = 0; i < string.length; i++) {
+            totalItemNumber = string.split(",").length;
+    }
+        return totalItemNumber;
+    }
 })();
